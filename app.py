452f7331aa0cd73704ec9ed95c921426eed5c20a
Ксеша <leafeon120@gmail.com>
2024-10-22 21:24:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ for _ in range (4):
 """
 Вывод игрового поля
 """
-def game_print():
+def game_print(game_field):
     for row in game_field:
         for symbol in row:
             print("{:^2}".format(symbol), end = '')
@@ -19,14 +19,15 @@ points = [0]*3
 def game_points(game_field):
     game_field = [['.'] * 5] + game_field + [['.'] * 5]
     for number in range (6):
-        game_field[number] = ['.'] + game_field[number] + ['.']  
+        game_field[number] = ['.'] + game_field[number] + ['.']
     for string in range(1, 5):
         for column in range(1, 6):
             count = 0
             for s_shift in range(-1, 2):
                 for c_shift in range(-1, 2):
-                    if s_shift != 0 and c_shift != 0 and game_field[string][column] == game_field[string + s_shift][column + c_shift]:
-                        count += 1
+                    if game_field[string][column] == game_field[string + s_shift][column + c_shift]:
+                        if (s_shift != 0 and c_shift != 0) or (s_shift == 0 and c_shift != 0) or (s_shift != 0 and c_shift == 0):
+                            count += 1
             points[gamers.index(game_field[string][column])] += count
             game_field[string][column] = 'o'
     return points
@@ -44,7 +45,7 @@ def game_go():
             int(game_field[string-1][column-1], 30)
             game_field[string-1][column-1] = gamers[step%3] 
             step += 1
-            game_print(); game_go()
+            game_print(game_field); game_go()
         except:
             print('Некорректный ввод! Попробуйте снова.')
             game_go()
@@ -55,5 +56,5 @@ def game_go():
             if points[point] == min(points):
                 winners.append(gamers[point]) 
         print('Победил(и) игрок(и)', *winners,'очки каждого игрока:', *points)
-game_print()
+game_print(game_field)
 game_go()
